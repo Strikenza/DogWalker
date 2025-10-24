8502e6adf822d5897d7a48f2639acfb4bb5ad541
Strikenza <stevent190@nycstudents.net>
2025-10-23 20:47:09 -0400

    done challenge

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,7 @@ import java.io.File;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-            DogWalkCompany c = new DogWalkCompany();
-            System.out.println(c);
-            int hour = c.addDogs();
-            System.out.println(c);
-            for (int i = 7; i < 11; i ++)
-                c.addDogs();
-            DogWalker w = new DogWalker(4, c);
-            w = new DogWalker(3,c);
-            System.out.println(w.walkDogs(hour) + " dogs walked");
-            System.out.println(c.numAvailableDogs(hour) + "dogs remaining");
-            System.out.println(c);
-            System.out.println("$" + w.dogWalkShift(7,10) + ".00 earned");
+        System.out.println(read());
         }
         public static int read() throws FileNotFoundException {
             int money = 0;
