4a0297b7f2ecf36c7a46d15896ac3762ca07db75
Strikenza <stevent190@nycstudents.net>
2025-10-23 20:53:44 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,19 @@ import java.io.FileNotFoundException;
 import java.io.File;
 import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
-        System.out.println(read());
+    public static void main(String[] args){
+            DogWalkCompany c = new DogWalkCompany();
+            System.out.println(c);
+            int hour = c.addDogs();
+            System.out.println(c);
+            for (int i = 7; i < 11; i ++)
+                c.addDogs();
+            DogWalker w = new DogWalker(4, c);
+            w = new DogWalker(3,c);
+            System.out.println(w.walkDogs(hour) + " dogs walked");
+            System.out.println(c.numAvailableDogs(hour) + "dogs remaining");
+            System.out.println(c);
+            System.out.println("$" + w.dogWalkShift(7,10) + ".00 earned");
         }
         public static int read() throws FileNotFoundException {
             int money = 0;
@@ -31,6 +42,7 @@ public class Main {
                 index++;
             }
             return money;
+            // just for challenge, ignore
         }
     }
 
